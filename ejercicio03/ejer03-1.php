42974ec29fadc4cafb7ddb8ea0804a25b8e34ee9
lore1 <lorena.araroj@educa.jcyl.es>
2022-10-07 11:22:20 +0000

    ejercicio3

--- ejercicio03/ejer03-1.php
+++ ejercicio03/ejer03-1.php
@@ -5,4 +5,11 @@
     echo "Tu dirección IP es: <strong> {$_SERVER['REMOTE_ADDR']} </strong><br>";
     //Mostrar el path donde se encuentra el fichero que se esta ejecutando
     echo "El path es: <strong>" .getcwd(). "</strong><br>";
+    //Mostrar fecha y hora en Oporto 
+    date_default_timezone_set('Europe/Lisbon');
+    echo "En oporto es: <strong>". date("D d m Y h:i:s"). "</strong><br>";
+    //inicializa y muestra una variable timestamp y en fecha de tu cumpleaños
+   $fecha = new DateTime("2001-09-27");
+   echo "Variable timestamp <strong>".date_timestamp_get($fecha). "</strong><br>";
+    
 ?>

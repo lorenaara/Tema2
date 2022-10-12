3f9a3b7302765d91a12593a77bbb9120ef1d026b
lore <lorena.araroj@educa.jcyl.es>
2022-10-12 08:50:13 +0000

    te

--- ejercicio03/ejer03-5.php
+++ ejercicio03/ejer03-5.php
@@ -1,11 +1,11 @@
 <?php
     include('html/plantilla.html');
-    $persona1=new DateTime($_GET['ano']."/". $_GET['mes']. "/". $_GET['dia']);
+   $persona1=new DateTime($_GET['ano'].'/'. $_GET['mes']. '/'. $_GET['dia']);
     $persona2=new DateTime( $_GET['ano2']. "/". $_GET['mes2']. "/". $_GET['dia2']);
+     echo $persona1." nació el ".Date("d/m/Y", mktime(0,0,0,$_GET["mes"],$_GET["dia"],$_GET["ano"]))."</br>";
     $resultado=$persona1->diff($persona2);
 
     echo abs($resultado->y);
-
 ?>
 <br> <div>
 <a href="ejer03-3.php">Codigo de la pagina</a>

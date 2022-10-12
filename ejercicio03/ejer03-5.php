d46f34da1299df8c9ff274afebf3bc69276710a4
lore <lorena.araroj@educa.jcyl.es>
2022-10-12 08:40:15 +0000

    terminado

--- ejercicio03/ejer03-5.php
+++ ejercicio03/ejer03-5.php
@@ -1,20 +1,11 @@
 <?php
     include('html/plantilla.html');
-    /*$persona1=new DateTime($_GET['fecha1']);
-    $persona2=new DateTime( $_GET['fecha2']);
+    $persona1=new DateTime($_GET['ano']."/". $_GET['mes']. "/". $_GET['dia']);
+    $persona2=new DateTime( $_GET['ano2']. "/". $_GET['mes2']. "/". $_GET['dia2']);
     $resultado=$persona1->diff($persona2);
 
-    echo $resultado->days .'días';*/
+    echo abs($resultado->y);
 
-    $persona1=$_GET['fecha1'];
-    $persona2=$_GET['fecha2'];
-    $persona1=strtotime($persona1);
-    $persona2=strtotime($persona2);
-    $resultado=abs($persona1-$persona2);
-    $anos= floor($resultado/(365*60*60*24));
-    $meses =floor($resultado-$anos*365*60*60*24)/(30*60*60*24);
-    $dias= floor($resultado-$anos*365*60*60*24 -$meses*30*60*60*24)/(60*60*24);
-    echo $anos . "años". $meses ."meses". $dias ."dias";
 ?>
 <br> <div>
 <a href="ejer03-3.php">Codigo de la pagina</a>
